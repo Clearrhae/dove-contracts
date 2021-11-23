55d4fc34d7686eacee6cd942637c2a333ae48c2f
fujita415 <fujita.hyo415@gmail.com>
2021-11-23 07:17:44 -0600

    deploy on Cronos testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,39 @@
-require('@nomiclabs/hardhat-waffle')
-require('@atixlabs/hardhat-time-n-mine')
-require('@nomiclabs/hardhat-etherscan')
+require("dotenv").config();
+require("@nomiclabs/hardhat-waffle");
 
-const { ethers } = require('ethers')
-const fs = require('fs')
-const dev = fs.readFileSync('.secret').toString().trim()
-const deployer = fs.readFileSync('.secret.mainnet').toString().trim()
-const etherscanApiKey = fs.readFileSync('.secret.etherscan').toString().trim()
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+const getHDWallet = () => {
+  const { MNEMONIC, PRIVATE_KEY } = process.env;
+  if (MNEMONIC && MNEMONIC !== "") {
+    return {
+      mnemonic: MNEMONIC,
+    }
+  }
+  if (PRIVATE_KEY && PRIVATE_KEY !== "") {
+    return [PRIVATE_KEY]
+  }
+  throw Error("Private Key Not Set! Please set up .env");
+}
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 
 module.exports = {
+  networks: {
+     cronos: {
+       url: "https://cronos-testnet-3.crypto.org:8545", 
+       accounts: getHDWallet(),
+       gas: 'auto',
+     },
+  },
   solidity: {
     compilers: [
       {
@@ -37,31 +62,4 @@ module.exports = {
       },
     ],
   },
-  networks: {
-    'polygon-mainnet': {
-      url: 'https://polygon-rpc.com',
-      accounts: [deployer],
-      gasPrice: 35000000000,
-      // gas: 20000000,
-    },
-    'polygon-mumbai': {
-      url: 'https://polygon-mumbai.infura.io/v3/d7dae60b5e1d40b9b31767b0086aa75d',
-      accounts: [dev],
-      gas: 'auto',
-      gasPrice: ethers.utils.parseUnits('1.2', 'gwei').toNumber(),
-    },
-    hardhat: {
-      gas: 'auto',
-      chainId: 137,
-      forking: {
-        url: 'https://polygon-rpc.com',
-      },
-    },
-  },
-  etherscan: {
-    apiKey: etherscanApiKey,
-  },
-  mocha: {
-    timeout: 5 * 60 * 10000,
-  },
-}
+};
